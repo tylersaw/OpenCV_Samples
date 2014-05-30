96f6784b3f743397bfc0ebab3107b35d13ed9765
root <root@tylersaw-ThinkPad-X60s.(none)>
2014-05-30 16:26:42 +0200

    linux-version-added

---
+++ video_read/video_read_linux/CMakeFiles/video_read.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/video_read.dir/video_read.o"
+  "video_read.pdb"
+  "video_read"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/video_read.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
